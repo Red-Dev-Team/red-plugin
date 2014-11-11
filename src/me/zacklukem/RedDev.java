53cb142d250edae5bb3f923da95eab43e44f1d12
Zachary Mayhew <mayhew.zachary2003@gmail.com>
2014-11-11 14:17:07 -0800

    Moved All Files to repository

---
+++ src/me/zacklukem/RedDev.java
@@ -0,0 +1,15 @@
+package me.zacklukem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RedDev extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+}
